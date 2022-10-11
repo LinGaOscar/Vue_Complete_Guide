000bdd871d9ac6e5a997d225c9f94288035c8152
LinGaOscar <oscar987123@yahoo.com.tw>
2022-10-11 17:50:42 +0800

    SET

---
+++ vue-cli-01-a-new-vue-project/src/component/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="togglesDeatils">
+      {{ detailsAreVisable ? "Hide" : "Show" }}Details
+    </button>
+    <ul v-if="detailsAreVisable">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisable: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123456789",
+        email: "sss@dff.com",
+      },
+    };
+  },
+  methods: {
+    togglesDeatils() {
+      this.detailsAreVisable = !this.detailsAreVisable;
+    },
+  },
+};
+</script>
