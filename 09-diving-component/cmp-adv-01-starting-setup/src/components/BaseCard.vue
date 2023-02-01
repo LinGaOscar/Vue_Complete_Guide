53477c05fe22d852a34c9c44070996bb64ee8cec
LinGaOscar <oscar987123@yahoo.com.tw>
2023-02-01 11:04:27 +0800

    slot style

--- 09-diving-component/cmp-adv-01-starting-setup/src/components/BaseCard.vue
+++ 09-diving-component/cmp-adv-01-starting-setup/src/components/BaseCard.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <header>
-      <slot name="header"></slot>
+      <slot name="header">
+        <h2>The Default Header</h2>
+      </slot>
     </header>
-    <slot></slot>
+    <slot>
+      <p>The Default Content with default</p>
+    </slot>
   </div>
 </template>
 
@@ -18,5 +22,10 @@ div {
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
