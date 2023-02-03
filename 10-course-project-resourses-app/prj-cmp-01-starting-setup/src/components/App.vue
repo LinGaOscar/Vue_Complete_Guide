18c57f1f49ca2da84ab47e79352aca11322ef5a8
LinGaOscar <oscar987123@yahoo.com.tw>
2023-02-03 16:21:45 +0800

    set project
    Create App.vue

---
+++ 10-course-project-resourses-app/prj-cmp-01-starting-setup/src/components/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        },
+
+      ]
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
