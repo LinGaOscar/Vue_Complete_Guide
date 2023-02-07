00071a1a2a75dd24497ec960b40fccf768061f94
LinGaOscar <oscar987123@yahoo.com.tw>
2023-02-07 15:51:01 +0800

    build delete button with provider and inject

--- 10-course-project-resourses-app/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
+++ 10-course-project-resourses-app/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <base-button mode="flat">Delete</base-button>
+        <base-button mode="flat" @click="removeResource(id)">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
@@ -14,12 +14,11 @@
 </template>
 
 <script>
-import BaseButton from "@/components/UI/BaseButton";
 export default {
-  components: {BaseButton},
   props: {
-    title: String, description: String, link: String
-  }
+    id: String, title: String, description: String, link: String
+  },
+  inject: ['removeResource']
 }
 </script>
 
