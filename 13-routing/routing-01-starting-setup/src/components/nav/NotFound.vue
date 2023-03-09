ef3b476eaec595ac1192c447fc1fb3671015c354
LinGaOscar <oscar987123@yahoo.com.tw>
2023-03-09 15:32:17 +0800

    redirection & catch all route
    invalid route

---
+++ 13-routing/routing-01-starting-setup/src/components/nav/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Page Not Found! Maybe view out
+    <router-link to="/teams">Teams</router-link>
+  </h2>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
--- 13-routing/routing-01-starting-setup/src/main.js
+++ 13-routing/routing-01-starting-setup/src/main.js
@@ -5,13 +5,17 @@ import App from './App.vue';
 import TeamsList from "@/components/teams/TeamsList";
 import UsersList from "@/components/users/UsersList";
 import TeamMembers from "@/components/teams/TeamMembers";
+import NotFound from "@/components/nav/NotFound";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {path: '/', redirect: '/teams'},
+        // {path: '/teams', component: TeamsList,alias:'/'},//our-domain.com/teams => TeamsList
         {path: '/teams', component: TeamsList},//our-domain.com/teams => TeamsList
         {path: '/users', component: UsersList},
         {path: '/teams/:teamId', component: TeamMembers, props: true},
+        {path: '/:notFound(.*)', component: NotFound}
     ],
     linkActiveClass: 'active',
     linkExactActiveClass: 'exact-active-link'
