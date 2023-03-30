a7fce7ea968ce5f68bdfb4c56260fb4d5d9a3345
LinGaOscar <oscar987123@yahoo.com.tw>
2023-03-30 16:15:43 +0800

    add more state

--- 15-vuex/vuex-01-starting-setup/src/main.js
+++ 15-vuex/vuex-01-starting-setup/src/main.js
@@ -4,46 +4,64 @@ import {createStore,} from 'vuex';
 import App from './App.vue';
 
 const store = createStore({
-    state() {
-        return {
-            counter: 0,
-        };
-    },
-    mutations: {
-        increment(state) {
-            state.counter = state.counter + 1;
+        state() {
+            return {
+                counter: 0,
+                isLoggedIn: false
+            };
         },
-        increase(state, payload) {
-            state.counter = state.counter + payload.value;
-        }
-    },
-    actions: {
-        increment(context) {
-            setTimeout(function () {
-                context.commit('increment');
-            }, 1500)
-        },
-        increase(context, payload) {
-            console.log(context);
-            context.commit('increase', payload);
-        }
-    },
-    getters: {
-        finalCounter(state) {
-            return state.counter;
-        },
-        normalizedCounter(_, getters) {
-            const finalCounter = getters.finalCounter;
-            if (finalCounter < 0) {
-                return 0;
+        mutations: {
+            increment(state) {
+                state.counter = state.counter + 1;
+            },
+            increase(state, payload) {
+                state.counter = state.counter + payload.value;
+            },
+            setAuth(state, payload) {
+                state.isLoggedIn = payload.isAuth;
             }
-            if (finalCounter > 100) {
-                return 100;
+        },
+        actions: {
+            increment(context) {
+                setTimeout(function () {
+                    context.commit('increment');
+                }, 1500)
+            },
+            increase(context, payload) {
+                console.log(context);
+                context.commit('increase', payload);
+            },
+            login(context) {
+                context.commit('setAuth', {
+                    isAuth: true
+                });
+            },
+            logout(context) {
+                context.commit('setAuth', {
+                    isAuth: false
+                });
+            },
+        },
+        getters: {
+            finalCounter(state) {
+                return state.counter;
+            },
+            normalizedCounter(_, getters) {
+                const finalCounter = getters.finalCounter;
+                if (finalCounter < 0) {
+                    return 0;
+                }
+                if (finalCounter > 100) {
+                    return 100;
+                }
+                return finalCounter;
+            },
+            userIsAuthenticated(state) {
+                return state.isLoggedIn;
             }
-            return finalCounter;
         }
-    }
-});
+    })
+;
 
 const app = createApp(App);
 
