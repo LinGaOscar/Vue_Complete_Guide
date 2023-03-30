669662588960a7ebe780e99792d734fabf3be26a
LinGaOscar <oscar987123@yahoo.com.tw>
2023-03-30 16:23:24 +0800

    store with module

--- 15-vuex/vuex-01-starting-setup/src/main.js
+++ 15-vuex/vuex-01-starting-setup/src/main.js
@@ -3,34 +3,64 @@ import {createStore,} from 'vuex';
 
 import App from './App.vue';
 
+const counterModule = {
+
+    state() {
+        return {
+            counter: 0
+        }
+    },
+    mutations: {
+        increment(state) {
+            state.counter = state.counter + 1;
+        },
+        increase(state, payload) {
+            state.counter = state.counter + payload.value;
+        },
+    },
+    actions: {
+        increment(context) {
+            setTimeout(function () {
+                context.commit('increment');
+            }, 1500)
+        },
+        increase(context, payload) {
+            console.log(context);
+            context.commit('increase', payload);
+        },
+    },
+    getters: {
+        finalCounter(state) {
+            return state.counter;
+        },
+        normalizedCounter(_, getters) {
+            const finalCounter = getters.finalCounter;
+            if (finalCounter < 0) {
+                return 0;
+            }
+            if (finalCounter > 100) {
+                return 100;
+            }
+            return finalCounter;
+        },
+    },
+}
+
 const store = createStore({
+        modules: {
+            numners: counterModule
+        },
         state() {
             return {
-                counter: 0,
                 isLoggedIn: false
             };
         },
         mutations: {
-            increment(state) {
-                state.counter = state.counter + 1;
-            },
-            increase(state, payload) {
-                state.counter = state.counter + payload.value;
-            },
             setAuth(state, payload) {
                 state.isLoggedIn = payload.isAuth;
             }
         },
         actions: {
-            increment(context) {
-                setTimeout(function () {
-                    context.commit('increment');
-                }, 1500)
-            },
-            increase(context, payload) {
-                console.log(context);
-                context.commit('increase', payload);
-            },
             login(context) {
                 context.commit('setAuth', {
                     isAuth: true
@@ -43,19 +73,6 @@ const store = createStore({
             },
         },
         getters: {
-            finalCounter(state) {
-                return state.counter;
-            },
-            normalizedCounter(_, getters) {
-                const finalCounter = getters.finalCounter;
-                if (finalCounter < 0) {
-                    return 0;
-                }
-                if (finalCounter > 100) {
-                    return 100;
-                }
-                return finalCounter;
-            },
             userIsAuthenticated(state) {
                 return state.isLoggedIn;
             }
