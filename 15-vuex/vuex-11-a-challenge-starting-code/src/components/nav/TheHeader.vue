05c44c8196657c2253923dfcc790e95afb1ee279
LinGaOscar <oscar987123@yahoo.com.tw>
2023-04-10 17:58:19 +0800

    store login

--- 15-vuex/vuex-11-a-challenge-starting-code/src/components/nav/TheHeader.vue
+++ 15-vuex/vuex-11-a-challenge-starting-code/src/components/nav/TheHeader.vue
@@ -29,6 +29,17 @@ export default {
   computed: {
     cartsQuantity() {
       return this.$store.getters['carts/quantity'];
+    },
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    }
+  },
+  methods: {
+    login() {
+      this.$store.dispatch('login');
+    },
+    logout() {
+      this.$store.dispatch('logout');
     }
   }
 
