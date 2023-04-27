71d0c44f9bc9578d5f0e8fbc4b4ffd2ca38e695f
LinGaOscar <oscar987123@yahoo.com.tw>
2023-04-27 17:00:31 +0800

    storing requests message with vues

--- 16-course-project-web-app/main-prj-01-starting-setup/src/pages/store/index.js
+++ 16-course-project-web-app/main-prj-01-starting-setup/src/pages/store/index.js
@@ -1,17 +1,19 @@
 import {createStore} from "vuex";
 import coachesModule from "@/pages/store/modules/coaches/index";
+import requestModule from "@/pages/store/modules/request/index"
 
 const store = createStore({
     modules: {
         coaches: coachesModule,
+        requests: requestModule
     },
-    state(){
-        return{
-          userId: 'c3'
+    state() {
+        return {
+            userId: 'c3'
         };
     },
-    getters:{
-        userId(state){
+    getters: {
+        userId(state) {
             return state.userId;
         }
     }
--- 16-course-project-web-app/main-prj-01-starting-setup/src/pages/store/modules/request/index.js
+++ 16-course-project-web-app/main-prj-01-starting-setup/src/pages/store/modules/request/index.js
@@ -1,1 +1,15 @@
-export default {}
+import mutations from "@/pages/store/modules/request/mutations";
+import actions from "@/pages/store/modules/request/actions";
+import getters from "@/pages/store/modules/request/getters";
+
+export default {
+    namespaced: true,
+    state() {
+        return {
+            requests: []
+        };
+    },
+    mutations,
+    actions,
+    getters
+}
