14733fb56202132ebd37be7ef34bfc309a2a8070
LinGaOscar <oscar987123@yahoo.com.tw>
2023-05-05 17:36:52 +0800

    signup action and flow

--- 16-course-project-web-app/main-prj-01-starting-setup/src/pages/store/modules/auth/actions.js
+++ 16-course-project-web-app/main-prj-01-starting-setup/src/pages/store/modules/auth/actions.js
@@ -1,1 +1,26 @@
-export default {};
+export default {
+    login() {},
+    async signup(context, payload) {
+        console.log(payload);
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB-oBCA9j00H6hXo8f_nDALm400boUpIE8', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to Authenticate');
+            throw  error;
+        }
+
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiration: responseData.expiresIn
+        });
+    }
+};
