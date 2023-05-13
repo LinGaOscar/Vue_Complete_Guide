bd70d0d87eb9ff58ea31f79f344feb72bed3b6e1
LinGaOscar <oscar987123@yahoo.com.tw>
2023-05-13 22:26:21 +0800

    use asynchronous components
    use asynchronous components

--- 16-course-project-web-app/main-prj-01-starting-setup/src/router.js
+++ 16-course-project-web-app/main-prj-01-starting-setup/src/router.js
@@ -1,13 +1,20 @@
+import {defineAsyncComponent} from "vue";
 import {createRouter, createWebHistory} from "vue-router";
 
-import CoachDetail from "@/pages/coaches/CoachDetail";
+// import CoachDetail from "@/pages/coaches/CoachDetail";
 import CoachesList from "@/pages/coaches/CoachesList";
-import CoachRegistration from "@/pages/coaches/CoachRegistration";
-import ContactCoach from "@/pages/request/ContactCoach";
-import RequestReceived from "@/pages/request/RequestReceived";
-import UserAuth from "@/pages/auth/UserAuth";
+// import CoachRegistration from "@/pages/coaches/CoachRegistration";
+// import ContactCoach from "@/pages/request/ContactCoach";
+// import RequestReceived from "@/pages/request/RequestReceived";
+// import UserAuth from "@/pages/auth/UserAuth";
 import NotFound from "@/pages/NotFound";
 import store from "@/store";
+
+const CoachDetail = () => import('@/pages/coaches/CoachDetail');
+const CoachRegistration = defineAsyncComponent(() => import('@/pages/coaches/CoachRegistration'));
+const ContactCoach = defineAsyncComponent(() => import('@/pages/request/ContactCoach'));
+const RequestReceived = defineAsyncComponent(() => import('@/pages/request/RequestReceived'));
+const UserAuth = defineAsyncComponent(() => import('@/pages/auth/UserAuth'));
 
 const router = createRouter({
     history: createWebHistory(),
